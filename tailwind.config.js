648a5c32fefdfa6df4d7fe8270c2a01d252408bf
fanjunming <fanfangbo@foxmail.com>
2026-08-03 19:01:28 +0800

    fix(Docs): exclude generated files from Tailwind scan

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,12 @@ module.exports = {
     './packages/docs/**/*.{md,ts,vue}',
     './packages/docs/.vitepress/**/*.{js,ts,vue}',
     './packages/docs/.vitepress/theme/**/*.{js,ts,vue}',
+    '!./packages/docs/.vitepress/dist/**',
+    '!./packages/docs/.vitepress/cache/**',
+    '!./packages/docs/.vitepress/.temp/**',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-  
