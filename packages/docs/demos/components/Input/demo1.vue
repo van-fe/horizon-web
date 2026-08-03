bcc91716773476a6c95927d019354c616c7dfc5b
fanjunming <fanfangbo@foxmail.com>
2026-08-03 16:58:32 +0800

    fix(Docs): restore Input demo radio labels

--- packages/docs/demos/components/Input/demo1.vue
+++ packages/docs/demos/components/Input/demo1.vue
@@ -3,25 +3,19 @@
     <h-form-item label="style">
       <h-radio-group v-model="inputStyle">
         <h-radio
-          v-for="(label, index) in ['normal', 'no-border', 'emphasize']"
-          :key="index"
-          :label="label"
-          size="small"
+          v-for="label in ['normal', 'no-border', 'emphasize']"
+          :key="label"
+          :value="label"
         />
       </h-radio-group>
     </h-form-item>
     <h-form-item label="disabled">
       <h-radio-group v-model="inputDisabled">
-        <h-radio
-          v-for="(label, index) in ['disabled', 'useable']"
-          :key="index"
-          :label="label"
-          size="small"
-        />
+        <h-radio v-for="label in ['disabled', 'enabled']" :key="label" :value="label" />
       </h-radio-group>
     </h-form-item>
     <h-form-item label="Label">
-      <h-input v-model="val" :input-style = 'inputStyle' :disabled="inputDisabled === 'disabled'"/>
+      <h-input v-model="val" :input-style="inputStyle" :disabled="inputDisabled === 'disabled'" />
     </h-form-item>
   </h-form>
 </template>
@@ -32,11 +26,11 @@ export default defineComponent({
   setup() {
     const val = ref('');
     const inputStyle = ref('normal');
-    const inputDisabled = ref('useable');
+    const inputDisabled = ref('enabled');
     return {
       val,
       inputStyle,
-      inputDisabled
+      inputDisabled,
     };
   },
 });
--- packages/docs/demos/components/Input/size.vue
+++ packages/docs/demos/components/Input/size.vue
@@ -2,30 +2,29 @@
   <h-form>
     <h-grid :gap="12">
       <h-form-item label="style">
-      <h-radio-group v-model="inputStyle">
-        <h-radio
-          v-for="(label, index) in ['normal', 'no-border', 'emphasize']"
-          :key="index"
-          :label="label"
-          size="small"
-        />
-      </h-radio-group>
-    </h-form-item>
+        <h-radio-group v-model="inputStyle">
+          <h-radio
+            v-for="label in ['normal', 'no-border', 'emphasize']"
+            :key="label"
+            :value="label"
+          />
+        </h-radio-group>
+      </h-form-item>
     </h-grid>
     <h-grid :gap="12">
       <h-grid-item :span="8">
         <h-form-item label="Large">
-          <h-input v-model="val1" size="large" :input-style = 'inputStyle'/>
+          <h-input v-model="val1" size="large" :input-style="inputStyle" />
         </h-form-item>
       </h-grid-item>
       <h-grid-item :span="8">
         <h-form-item label="Medium">
-          <h-input v-model="val2" size="medium" :input-style = 'inputStyle'/>
+          <h-input v-model="val2" size="medium" :input-style="inputStyle" />
         </h-form-item>
       </h-grid-item>
       <h-grid-item :span="8">
         <h-form-item label="Small">
-          <h-input v-model="val3" size="small" :input-style = 'inputStyle'/>
+          <h-input v-model="val3" size="small" :input-style="inputStyle" />
         </h-form-item>
       </h-grid-item>
     </h-grid>
