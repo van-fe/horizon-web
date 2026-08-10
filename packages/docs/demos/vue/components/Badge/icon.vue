4660ea2f433a5cb1d72429a07fc041e3a0ff027d
fanjunming <fanfangbo@foxmail.com>
2026-08-10 15:42:37 +0800

    fix(Badge): overlay icon on avatar corner

--- packages/docs/demos/vue/components/Badge/icon.vue
+++ packages/docs/demos/vue/components/Badge/icon.vue
@@ -6,6 +6,7 @@
         content="owner_os"
         :icon-size="item.iconSize"
         icon-color="var(--h-text-brand-default)"
+        align="inner"
         bottom
       >
         <h-avatar type="work" :src="item.avatar" :size="item.size" />
